146877c73098783c11b4490128a1c535493ce67c
Victor Menezes do Vale <153623318+ovictorvale@users.noreply.github.com>
2026-06-04 17:08:16 -0300

    feat: atenticacao inicial com usuarios mysql

--- lib/eco_habits_web/router.ex
+++ lib/eco_habits_web/router.ex
@@ -1,6 +1,8 @@
 defmodule EcoHabitsWeb.Router do
   use EcoHabitsWeb, :router
 
+  import EcoHabitsWeb.UserAuth
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
@@ -8,6 +10,7 @@ defmodule EcoHabitsWeb.Router do
     plug :put_root_layout, html: {EcoHabitsWeb.Layouts, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug :fetch_current_scope_for_user
   end
 
   pipeline :api do
@@ -40,5 +43,33 @@ defmodule EcoHabitsWeb.Router do
       live_dashboard "/dashboard", metrics: EcoHabitsWeb.Telemetry
       forward "/mailbox", Plug.Swoosh.MailboxPreview
     end
+  end
+
+  ## Authentication routes
+
+  scope "/", EcoHabitsWeb do
+    pipe_through [:browser, :require_authenticated_user]
+
+    live_session :require_authenticated_user,
+      on_mount: [{EcoHabitsWeb.UserAuth, :require_authenticated}] do
+      live "/users/settings", UserLive.Settings, :edit
+      live "/users/settings/confirm-email/:token", UserLive.Settings, :confirm_email
+    end
+
+    post "/users/update-password", UserSessionController, :update_password
+  end
+
+  scope "/", EcoHabitsWeb do
+    pipe_through [:browser]
+
+    live_session :current_user,
+      on_mount: [{EcoHabitsWeb.UserAuth, :mount_current_scope}] do
+      live "/users/register", UserLive.Registration, :new
+      live "/users/log-in", UserLive.Login, :new
+      live "/users/log-in/:token", UserLive.Confirmation, :new
+    end
+
+    post "/users/log-in", UserSessionController, :create
+    delete "/users/log-out", UserSessionController, :delete
   end
 end
