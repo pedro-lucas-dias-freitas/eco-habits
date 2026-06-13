989ccdfb13cbc2e1501c7113765fb0c1fb8e082d
Victor Menezes do Vale <153623318+ovictorvale@users.noreply.github.com>
2026-06-13 17:36:46 -0300

    feat: remove elementos phoenix, ajustes de rotas

--- lib/eco_habits_web/controllers/page_controller.ex
+++ lib/eco_habits_web/controllers/page_controller.ex
@@ -2,6 +2,12 @@ defmodule EcoHabitsWeb.PageController do
   use EcoHabitsWeb, :controller
 
   def home(conn, _params) do
-    render(conn, :home)
+    case conn.assigns[:current_scope] do
+      %{user: user} when not is_nil(user) ->
+        redirect(conn, to: ~p"/profile")
+
+        _->
+          redirect(conn, to: ~p"/users/log-in")
+    end
   end
 end
